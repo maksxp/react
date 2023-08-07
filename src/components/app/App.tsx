47c27587967d430fda0bcb237ecccf4fdea8b41a
Maksym Murza <maksym.murza@ideus.biz>
2023-08-07 13:25:36 +0300

    fetching is enabled

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from '../../logo.svg';
 import './App.css';
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const url = `http://localhost:5000/news`
+
+  const getData = async () => {
+    fetch(url).then((resp) => {
+      console.log('RESP ', resp)
+    })
+    // try {
+    //   const response = await axios.get(url);
+    //   setData(response.data);
+    //   setError(null);
+    // } catch (err: any) {
+    //   setError(err.message);
+    //   setData(null);
+    // } finally {
+    //   setLoading(false);
+    // }
+  };
+
+  const saveData = async () => {
+    try {
+      const response = await axios.post(url);
+      setData(response.data);
+      setError(null);
+    } catch (err: any) {
+      setError(err.message);
+      setData(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // const url = `http://localhost:5000/news`
+    // const getData = async () => {
+    //   try {
+    //     const response = await axios.get(url);
+    //     setData(response.data);
+    //     setError(null);
+    //   } catch (err: any) {
+    //     setError(err.message);
+    //     setData(null);
+    //   } finally {
+    //     setLoading(false);
+    //   }
+    // };
+    getData();
+    // saveData();
+  }, [loading]);
+
   return (
-    <div className="App">
-      <header className="App-header">{process.env.REACT_APP_TEST_KEY}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="App">
+        {loading && <p>Loading...</p>}
+        {!loading && <p>Fetched data</p>}
+      </div>
+    </>
+  )
 }
 
 export default App;
